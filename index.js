07820155a29bd0b0eeae66bd3a45561a770f06b8
Bipisha <paudelbipisha4@gmail.com>
2026-09-01 21:19:13 +0545

    exercise 4: GET/POST/DELETE routes

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const { Sequelize } = require('./util/db')
+const Blog = require('./models/blog')
+
+app.use(express.json())
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try{
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({error})
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if(blog) {
+    await blog.destroy()
+  }
+  res.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}` )
+})
---
+++ models/blog.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize')
+const { sequelize } = require('../util/db')
+
+class Blog extends Model {}
+
+Blog.init({
+  author: {
+    type: DataTypes.TEXT 
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false 
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false 
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+module.exports = Blog 
