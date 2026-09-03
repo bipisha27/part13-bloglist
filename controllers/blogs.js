c3f128f6ac80be8914222c5befad15974bb76fd1
Bipisha <paudelbipisha4@gmail.com>
2026-09-03 19:16:28 +0545

    user model, email validation, blog ownership using JWT

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,25 +1,53 @@
+const jwt = require('jsonwebtoken')
 const router = require('express').Router()
-const {Blog} = require('../models')
+const {SECRET} = require('../util/config')
+const {Blog, User} = require('../models')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+
+  if(authorization && authorization.toLowerCase().startsWith('bearer')) {
+    try{
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({error: 'token invalid'})
+    }
+  } else {
+    return res.status(401).json({error: 'token missing'})
+  }
+  next()
+}
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({
+    attributes: {exclude: ['userId']},
+    include: {
+      model: User,
+      attributes: ['name']
+    }
+  })
   res.json(blogs)
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', tokenExtractor, async (req, res, next) => {
   try {
-    const blog = await Blog.create(req.body)
-    return res.json(blog)
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({...req.body, userId: user.id})
+    res.json(blog)
   } catch (error) {
     next(error)
   }
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', tokenExtractor, async (req, res) => {
   const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    await blog.destroy()
+  if (!blog) {
+    return res.status(204).end()
   }
+  if(blog.userId != req.decodedToken.id) {
+    return res.status(403).json({error: 'only the creator can delete this blog'})
+  }
+  await blog.destroy()
   res.status(204).end()
 })
 
