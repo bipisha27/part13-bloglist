9c3b0a26034e31708ffd648affc40e7e449259db
Bipisha <paudelbipisha4@gmail.com>
2026-09-03 13:24:25 +0545

    restructure, PUT likes route, centralized error handling

---
+++ controllers/blogs.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const Blog = require('../models/blog')
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    await blog.destroy()
+  }
+  res.status(204).end()
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+      blog.likes = req.body.likes
+      await blog.save()
+      res.json(blog)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
