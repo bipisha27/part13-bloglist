228489a356a1f660a1afa4ea3e3fe0e1999c1b9d
Bipisha <paudelbipisha4@gmail.com>
2026-09-04 12:01:08 +0545

    some fixes

--- util/db.js
+++ util/db.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-const DATABASE_URL = process.env.TESTING === 'true'
+const isTest = process.env.TESTING === 'true'
+
+const DATABASE_URL = isTest
   ? process.env.TEST_DATABASE_URL
   : process.env.DATABASE_URL
 
